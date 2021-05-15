10bebdecdcf7b25370a67570091dc1ec1586e859
Alex <aleksandr.kz2000@mail.ru>
2021-05-15 20:05:35 +0500

    now ws server is working

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const ws = require('ws');
+const express = require("express");
+const PORT = process.env.PORT || 777;
+const INDEX = '/client/index.html';
 
-const wss = new ws.Server({
-    port: 777
-}, () => console.log('ws server started'))
+const { Server } = require('ws');
 
-wss.on('connection', () => {
-    console.log('кто-то подключился');
+const server = express()
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
 });
+
+setInterval(() => {
+    wss.clients.forEach((client) => {
+        client.send(new Date().toTimeString());
+    });
+}, 1000);
